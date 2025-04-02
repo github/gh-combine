8ef505021b5d8598df15df2e51c274784a33f618
GrantBirki <grant.birkinbine@gmail.com>
2025-04-02 11:25:52 -0700

    make the debug logs a bit easier to read

--- internal/cmd/logger.go
+++ internal/cmd/logger.go
@@ -15,7 +15,7 @@ func init() {
 		logLevel = "info" // default to info level
 	}
 
-	// upcase the log level
+	// Upcase the log level
 	logLevel = strings.ToUpper(logLevel)
 
 	var level slog.Level
@@ -33,11 +33,26 @@ func init() {
 	}
 
 	opts := &slog.HandlerOptions{
-		Level: level,
+		Level:       level,
+		AddSource:   true,               // Include source file and line number for context
+		ReplaceAttr: humanFriendlyAttrs, // Customize log attributes for readability
 	}
 
 	handler := slog.NewTextHandler(os.Stdout, opts)
 
 	// Configure slog with the specified log level
 	Logger = slog.New(handler)
+}
+
+// humanFriendlyAttrs customizes log attributes for human readability
+func humanFriendlyAttrs(groups []string, a slog.Attr) slog.Attr {
+	switch a.Key {
+	case slog.TimeKey:
+		// Format time as a human-readable string
+		a.Value = slog.StringValue(a.Value.Time().Format("2006-01-02 15:04:05"))
+	case slog.LevelKey:
+		// Capitalize log levels for better visibility
+		a.Value = slog.StringValue(strings.ToUpper(a.Value.String()))
+	}
+	return a
 }
